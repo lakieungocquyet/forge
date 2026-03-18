8918f229943cf0f6188e01c692113309a48b3f2c
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-18 12:41:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+pixi workspace environment add forge
+
+pixi task add forge "python3 main.py" --cwd ${PIXI_PROJECT_ROOT}/src/ --description "Run Forge"
+echo 'alias forge="pixi run forge"' >> ~/.bashrc
+
 pixi install -e forge
 echo "# Added by forge installer" >> ~/.bashrc
 pixi shell-hook --shell bash -e forge >> ~/.bashrc
