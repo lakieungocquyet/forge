65c9bc94eedd212eeecf51fe08c6517b140cabd3
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-18 15:26:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
+pixi init .
+pixi workspace channel add -m pixi.toml "bioconda"
 pixi workspace environment add forge
 
+pixi add --no-install \
+    bwa=* samtools=* gatk4=* bcftools=* snpeff=* snpsift=* \
+    jq=* python=* \
+    pyyaml=* pandas=* xlsxwriter=* seaborn=* cyvcf2=* 
+
 pixi task add forge "python3 main.py" --cwd ${PIXI_PROJECT_ROOT}/src/ --description "Run Forge"
-echo 'alias forge="pixi run forge"' >> ~/.bashrc
 
 pixi install -e forge
-echo "# Added by forge installer" >> ~/.bashrc
+
+echo "# >>> added by forge installer >>>" >> ~/.bashrc
+
+echo 'alias forge="pixi run --quiet forge"' >> ~/.bashrc
+
+echo "# >>> forge shell hook >>>" >> ~/.bashrc
 pixi shell-hook --shell bash -e forge >> ~/.bashrc
+echo "# <<< forge shell hook <<<" >> ~/.bashrc
+
+echo "# <<< added by forge installer <<<" >> ~/.bashrc
+
+source ~/.bashrc
