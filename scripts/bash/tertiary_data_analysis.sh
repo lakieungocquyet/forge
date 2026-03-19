a63f6818c73dea27ef1028b9006c3c132df07993
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-19 17:03:19 +0700

    Update tertiary_data_analysis.sh

--- scripts/bash/tertiary_data_analysis.sh
+++ scripts/bash/tertiary_data_analysis.sh
@@ -2,8 +2,8 @@ SCRIPT_DIR_PATH="$(dirname "$(realpath $0)")"
 
 source "$SCRIPT_DIR_PATH/../../src/utils/setup_logging.sh"
 
-INPUT_SAMPLE_LIST=$(echo "$1" | jq -r ".input_data.input.sample")
-OUTPUT_DIR_PATH=$(echo "$1" | jq -r ".input_data.output.directory")
+INPUT_SAMPLE_LIST=$(echo "$1" | jq -r ".input_data.sample")
+OUTPUT_DIR_PATH=$(echo "$1" | jq -r ".output_dir_path")
 
 
 
@@ -11,18 +11,18 @@ RUNTIME_LOG_FILE_PATH="${SCRIPT_DIR_PATH}/../../log/runtime.log"
 MONITORING_LOG_FILE_PATH="${SCRIPT_DIR_PATH}/../../log/monitoring.log"
 
 
-REFERENCE_GENOME_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r ".config_data.resources.hg19.reference_genome")"
+REFERENCE_GENOME_FILE_PATH="$(echo "$1" | jq -r ".config_data.resources.reference_genome_file_path")"
 
 
-G1000_OMNI2_5_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources["1000g_omni2_5"]')"
-DBNSFP4_9A_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources.dbnsfp4_9a')"
-DBSNP_138_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources.dbsnp_138')"
-G1000_PHASE1_INDELS_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources["1000g_phase1_indels"]')"
-G1000_PHASE3_V4_20130502_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources["1000g_phase3_v4_20130502"]')"
-CLINVAR_20240716_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources.clinvar_20240716')"
-ESP6500SI_V2_SSA137_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r '.config_data.resources.hg19.variant_resources.esp6500si_v2_ssa137')"
+OMNI2_5_1000G_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.omni2_5_1000g')"
+DBNSFP_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.dbnsfp')"
+DBSNP_138_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.dbsnp_138')"
+PHASE1_1000G_INDELS_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.phase1_1000g_indels')"
+PHASE3_1000G_V4_20130502_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.phase3_1000g_v4_20130502')"
+CLINVAR_20240716_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.clinvar')"
+ESP6500SI_V2_SSA137_FILE_PATH="$(echo "$1" | jq -r '.config_data.resources.annotation_resource_dict.esp6500si_v2_ssa137')"
 
-S07604624_REGIONS_FILE_PATH="${SCRIPT_DIR_PATH}/../../$(echo "$1" | jq -r ".config_data.resources.hg19.regions.s07604624")"
+REGIONS_FILE_PATH="$(echo "$1" | jq -r ".config_data.resources.regions_file_path")"
 
 THREADS=$(echo "$1" | jq -r ".config_data.compute.threads")
 MIN_MEMORY_GB=$(echo "$1" | jq -r ".config_data.compute.min_memory_gb")
@@ -42,51 +42,76 @@ logger INFO "annotate variant type"
         > ${OUTPUT_DIR_PATH}/annotation_temp_2.vcf \
 2>> ${MONITORING_LOG_FILE_PATH}
 
-logger INFO "annotate clinvar"
-/usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-    java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
-        -noId -name CLINVAR_ \
-        ${CLINVAR_20240716_FILE_PATH} \
-        ${OUTPUT_DIR_PATH}/annotation_temp_2.vcf \
-        > ${OUTPUT_DIR_PATH}/annotation_temp_3.vcf \
-2>> ${MONITORING_LOG_FILE_PATH}
+if [ -f "$CLINVAR_FILE_PATH" ]; then
+    logger INFO "annotate clinvar"
+    /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+        java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
+            -noId -name CLINVAR_ \
+            ${CLINVAR_FILE_PATH} \
+            ${OUTPUT_DIR_PATH}/annotation_temp_2.vcf \
+            > ${OUTPUT_DIR_PATH}/annotation_temp_3.vcf \
+    2>> ${MONITORING_LOG_FILE_PATH}
+else
+    logger WARN "skip clinvar annotation (file not found)"
+    cp "${OUTPUT_DIR_PATH}/annotation_temp_2.vcf" "${OUTPUT_DIR_PATH}/annotation_temp_3.vcf"
+fi
 
-logger INFO "annotate 1000g phase3"
-/usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-    java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
-        -noId -name p3_1000G_ \
-        ${G1000_PHASE3_V4_20130502_FILE_PATH} \
-        ${OUTPUT_DIR_PATH}/annotation_temp_3.vcf  \
-        > ${OUTPUT_DIR_PATH}/annotation_temp_4.vcf \
-2>> ${MONITORING_LOG_FILE_PATH}
+if [ -f "$PHASE3_1000G_V4_20130502_FILE_PATH" ]; then
+    logger INFO "annotate 1000g phase3"
+    /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+        java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
+            -noId -name p3_1000G_ \
+            ${PHASE3_1000G_V4_20130502_FILE_PATH} \
+            ${OUTPUT_DIR_PATH}/annotation_temp_3.vcf  \
+            > ${OUTPUT_DIR_PATH}/annotation_temp_4.vcf \
+    2>> ${MONITORING_LOG_FILE_PATH}
+else
+    logger WARN "skip 1000g phase3 annotation (file not found)"
+    cp "${OUTPUT_DIR_PATH}/annotation_temp_3.vcf" "${OUTPUT_DIR_PATH}/annotation_temp_4.vcf"
+fi
 
-logger INFO "annotate esp6500"
-/usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-    java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
-        -noId -name ESP6500_ \
-        ${ESP6500SI_V2_SSA137_FILE_PATH} \
-        ${OUTPUT_DIR_PATH}/annotation_temp_4.vcf  \
-        > ${OUTPUT_DIR_PATH}/annotation_temp_5.vcf \
-2>> ${MONITORING_LOG_FILE_PATH}
+if [ -f "$ESP6500SI_V2_SSA137_FILE_PATH" ]; then
+    logger INFO "annotate esp6500"
+    /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+        java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
+            -noId -name ESP6500_ \
+            ${ESP6500SI_V2_SSA137_FILE_PATH} \
+            ${OUTPUT_DIR_PATH}/annotation_temp_4.vcf  \
+            > ${OUTPUT_DIR_PATH}/annotation_temp_5.vcf \
+    2>> ${MONITORING_LOG_FILE_PATH}
+else
+    logger WARN "skip esp6500 annotation (file not found)"
+    cp "${OUTPUT_DIR_PATH}/annotation_temp_4.vcf" "${OUTPUT_DIR_PATH}/annotation_temp_5.vcf"
+fi
 
-logger INFO "annotate dbsnp"
-/usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-    java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
-        -noId -info dbSNP138_ID,dbSNPBuildID \
-        -id \
-        ${DBSNP_138_FILE_PATH} \
-        ${OUTPUT_DIR_PATH}/annotation_temp_5.vcf \
-        > ${OUTPUT_DIR_PATH}/annotation_temp_6.vcf \
-2>> ${MONITORING_LOG_FILE_PATH}
+if [ -f "$DBSNP_138_FILE_PATH" ]; then
+    logger INFO "annotate dbsnp138"
+    /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+        java -Xmx${MAX_MEMORY_GB}g -jar  ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar annotate \
+            -noId -info dbSNP138_ID,dbSNPBuildID \
+            -id \
+            ${DBSNP_138_FILE_PATH} \
+            ${OUTPUT_DIR_PATH}/annotation_temp_5.vcf \
+            > ${OUTPUT_DIR_PATH}/annotation_temp_6.vcf \
+    2>> ${MONITORING_LOG_FILE_PATH}
+else
+    logger WARN "skip dbsnp138 annotation (file not found)"
+    cp "${OUTPUT_DIR_PATH}/annotation_temp_5.vcf" "${OUTPUT_DIR_PATH}/annotation_temp_6.vcf"
+fi
 
-logger INFO "annotate dbnsfp"
-/usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-    java -Xmx${MAX_MEMORY_GB}g -jar  \
-        ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar dbnsfp -v -f "" -n \
-        -db ${DBNSFP4_9A_FILE_PATH} \
-        ${OUTPUT_DIR_PATH}/annotation_temp_6.vcf \
-        > ${OUTPUT_DIR_PATH}/annotation_temp_7.vcf \
-2>> ${MONITORING_LOG_FILE_PATH}
+if [ -f "$DBNSFP_FILE_PATH" ]; then
+    logger INFO "annotate dbnsfp"
+    /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+        java -Xmx${MAX_MEMORY_GB}g -jar  \
+            ${SCRIPT_DIR_PATH}/../../.pixi/envs/forge/share/snpsift-5.4.0c-0/SnpSift.jar dbnsfp -v -f "" -n \
+            -db ${DBNSFP_FILE_PATH} \
+            ${OUTPUT_DIR_PATH}/annotation_temp_6.vcf \
+            > ${OUTPUT_DIR_PATH}/annotation_temp_7.vcf \
+    2>> ${MONITORING_LOG_FILE_PATH}
+else
+    logger WARN "skip dbnsfp annotation (file not found)"
+    cp "${OUTPUT_DIR_PATH}/annotation_temp_6.vcf" "${OUTPUT_DIR_PATH}/annotation_temp_7.vcf"
+fi
 
 while read -r sample; do
     sample_id=$(echo "$sample" | jq -r ".id")
