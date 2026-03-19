414815be66b32b9f5b56422437719def4440126e
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-19 10:23:54 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
-sed -i '/>>> forge shell hook >>>/,/<<< forge shell hook <<</d' ~/.bashrc
+# sed -i '/>>> forge shell hook >>>/,/<<< forge shell hook <<</d' ~/.bashrc
 sed -i '/>>> added by forge installer >>>/,/<<< added by forge installer <<</d' ~/.bashrc
