a99c7ef5fed87a435d0f400ec223c3029d00208d
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-19 17:02:49 +0700

    Create example.sh

---
+++ example/example.sh
@@ -0,0 +1,5 @@
+forge callvariants \
+    -I /home/lknq/GitHub/forge/example/input.yaml \
+    -O /home/lknq/GitHub/forge/results \
+    -R /home/lknq/GitHub/forge/resources/hg19/reference_genome_hg19/hg19.p13.plusMT.no_alt_analysis_set.fa \
+    -r /home/lknq/GitHub/forge/resources/hg19/regions_hg19/s07604624_hg19/s07604624_covered.bed
