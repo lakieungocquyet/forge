7d20d2e78486ceb58b08072c250e0d6b26a4b95a
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-23 15:29:16 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,9 @@
+SCRIPT_DIR_PATH="$(dirname "$(realpath $0)")"
+
+rm -rf "$SCRIPT_DIR_PATH/.pixi"
+rm -f pixi.toml
+rm -f pixi.lock
+
 # sed -i '/>>> forge shell hook >>>/,/<<< forge shell hook <<</d' ~/.bashrc
 sed -i '/>>> added by forge installer >>>/,/<<< added by forge installer <<</d' ~/.bashrc
+
