a3f8beaafc23e3c828ebe164d2a243b5ac1c29f7
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-24 10:17:53 +0700

    Update identify_hla_alleles.sh

--- scripts/bash/identify_hla_alleles.sh
+++ scripts/bash/identify_hla_alleles.sh
@@ -1,6 +1,8 @@
 SCRIPT_DIR_PATH="$(dirname "$(realpath $0)")"
 
 source "$SCRIPT_DIR_PATH/../../src/bash/utils/setup_logging.sh"
+source "$SCRIPT_DIR_PATH/../../src/bash/utils/render_monitoring_window.sh"
+source "$SCRIPT_DIR_PATH/../../src/bash/utils/run_command_with_monitoring.sh"
 
 cyan_color="\e[36m"  # cyan
 green_color="\e[32m"   # green
@@ -20,29 +22,33 @@ MONITORING_LOG_FILE_PATH="$OUTPUT_DIR_PATH/log/monitoring.log"
 REFERENCE_HLA_DNA_FILE_PATH="$SCRIPT_DIR_PATH/../../resources/hla/hla_dna_seq.fa"
 REFERENCE_HLA_RNA_FILE_PATH="$SCRIPT_DIR_PATH/../../resources/hla/hla_rna_seq.fa"
 
-
 while read -r sample; do 
     sample_id=$(echo "$sample" | jq -r ".id")
     sample_platform=$(echo "$sample" | jq -r ".platform")
     sample_read1=$(echo "$sample" | jq -r ".read1")
     sample_read2=$(echo "$sample" | jq -r ".read2")
 
+    mkdir -p "$OUTPUT_DIR_PATH/$sample_id/"
     if [[ ! -f "$REFERENCE_HLA_DNA_FILE_PATH" || ! -f "$REFERENCE_HLA_RNA_FILE_PATH" ]]; then
         logger WARNING $MONITORING_LOG_FILE_PATH "Prepare HLA reference file (file not found)"
         t1k-build.pl --download IPD-IMGT/HLA --prefix hla -o "$SCRIPT_DIR_PATH/../../resources"
     else
         logger INFO $MONITORING_LOG_FILE_PATH "Identify HLA alleles in sample ${green_color}$sample_id${reset}"
-        /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
-            run-t1k --preset hla \
-                -1 "$sample_read1" \
-                -2 "$sample_read2" \
-                -f "$REFERENCE_HLA_DNA_FILE_PATH" \
-                -o "$OUTPUT_DIR_PATH/$sample_id" \
-        2>> ${MONITORING_LOG_FILE_PATH}
+   
+        run_command_with_monitoring "     
+            /usr/bin/time -v -a -o ${RUNTIME_LOG_FILE_PATH} \
+                run-t1k --preset hla \
+                    -1 "$sample_read1" \
+                    -2 "$sample_read2" \
+                    -f "$REFERENCE_HLA_DNA_FILE_PATH" \
+                    -o "$OUTPUT_DIR_PATH/$sample_id/$sample_id"
+        " ${MONITORING_LOG_FILE_PATH} "Identify HLA alleles in sample ${green_color}$sample_id${reset}"
 
         logger INFO $MONITORING_LOG_FILE_PATH "Generate HLA typing XLSX report for ${green_color}$sample_id${reset}"
-        python3 "${SCRIPT_DIR_PATH}/../python/generate_hla_typing_xlsx_report.py" \
-            -I "$OUTPUT_DIR_PATH/$sample_id/${sample_id}_genotype.tsv" \
-            -O "$OUTPUT_DIR_PATH/$sample_id/${sample_id}.hla_typing.xlsx"
+        run_command_with_monitoring " 
+            python3 "${SCRIPT_DIR_PATH}/../python/generate_hla_typing_xlsx_report.py" \
+                -I "$OUTPUT_DIR_PATH/$sample_id/${sample_id}_genotype.tsv" \
+                -O "$OUTPUT_DIR_PATH/$sample_id/${sample_id}.hla_typing.xlsx"
+        " ${MONITORING_LOG_FILE_PATH} "Generate HLA typing XLSX report for ${green_color}$sample_id${reset}"
     fi
 done < <(echo "$INPUT_SAMPLE_LIST" | jq -c '.[]')
