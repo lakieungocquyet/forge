70f1986b56fc1b20927ee4b90f718637ba34301f
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-25 22:18:03 +0700

    Update run_command_with_monitoring.sh

--- src/bash/utils/run_command_with_monitoring.sh
+++ src/bash/utils/run_command_with_monitoring.sh
@@ -5,23 +5,21 @@ run_command_with_monitoring() {
     local step_name="$4"
     : > "$ui_file"
 
-    # bash -c "$command" </dev/null 2>&1 \
-    #     | stdbuf -oL tee -a "$log_file" "$ui_file" >/dev/null &
+    stdbuf -oL -eL bash -c "$command" 2>&1 \
+        |stdbuf -oL tee -a "$log_file" "$ui_file" >/dev/null &
 
-    # bash -c "$command" </dev/null 2>&1 \
-    #     | tee -a "$log_file" "$ui_file" >/dev/null &
-
-    bash -c "$command" 2>&1 \
-        | tee -a "$log_file" "$ui_file" >/dev/null &
-    local command_pid=$!
-    
     local command_pid=$!
 
     render_monitoring_window "$ui_file" "$step_name" &
     local window_pid=$!
     
-    wait $command_pid
+    while kill -0 $command_pid 2>/dev/null; do
+        wait $command_pid 2>/dev/null
+    done
+
     kill $window_pid 2>/dev/null
+    wait $window_pid 2>/dev/null
+
     tput rc
     tput ed 
 }
