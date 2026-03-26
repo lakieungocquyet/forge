2be144ea723194cc186a4e696ba8097728dae2c4
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-03-26 21:47:40 +0700

    Update render_monitoring_window.sh

--- src/bash/utils/render_monitoring_window.sh
+++ src/bash/utils/render_monitoring_window.sh
@@ -1,5 +1,3 @@
-WINDOW_HEIGHT=11
-
 get_cursor_row() {
     local row col
     if IFS=';' read -t 0.1 -sdR -p $'\e[6n' row col < /dev/tty; then
@@ -16,7 +14,6 @@ cleanup() {
     trap - INT TERM EXIT
     tput cnorm
     tput rmcup 2>/dev/null
-
     echo -e "\n[!] Exit process!"
     exit 0
 }
@@ -37,16 +34,15 @@ trap 'cleanup_suspend; kill -TSTP $$' TSTP # Pause main process
 trap 'resume' CONT # Resume main process
 trap cleanup INT TERM EXIT # End main process
 
-
 render_monitoring_window() {
     local file=$1
     local step_name=$2
     local spin='|/-\'
-    local i=0
+    local s=0
 
     resized=true
     trap 'resized=true' WINCH # Listen resize in subprocess
-
+    
     tput smcup
     tput clear
     tput civis 
@@ -55,37 +51,61 @@ render_monitoring_window() {
     while true; do
         COLOR_BORDER="\e[33m"
         RESET="\e[0m"
+
+        rows=$(tput lines)
         cols=$(tput cols)
+
+        FIXED_LINES=2
+        MONITORING_WINDOW_HEIGHT=$(( $rows - $FIXED_LINES ))
+        MONITORING_CONTENT_HEIGHT=$(( $MONITORING_WINDOW_HEIGHT - 2 ))
+
         inner_width=$((cols - 4))
 
         if $resized; then
-            tput rc
-            tput ed
+            rows=$(tput lines)
+            FIXED_LINES=2
+            MONITORING_WINDOW_HEIGHT=$(( $rows - $FIXED_LINES ))
+            MONITORING_CONTENT_HEIGHT=$(( $MONITORING_WINDOW_HEIGHT - 2 ))
+            tput clear
             resized=false
         fi
 
         printf '\n'
-        printf "[%c] " "${spin:$i:1}"
+        printf "[%c] " "${spin:$s:1}"
         printf "Running %b\n" "$step_name"
-        i=$(( (i+1) %4 ))
+        s=$(( (s+1) %4 ))
 
-        title="[MONITORING]"
+        title=" MONITORING "
         title_len=${#title}
         remaining=$((cols - title_len - 2))
         left=4
         right=$((remaining - left))
 
-        printf "${COLOR_BORDER}+%*s%s%*s+${RESET}\n" "$left" '' "$title" "$right" '' | tr ' ' '-'
+        printf "${COLOR_BORDER}╭"
 
-        mapfile -t lines < <(tail -n $((WINDOW_HEIGHT-2)) "$file")
+        for ((i=0; i<left; i++)); do printf "─"; done
 
+        printf "%s" "$title"
+
+        for ((i=0; i<right; i++)); do printf "─"; done
+
+        printf "╮${RESET}\n"
+
+        mapfile -t lines < <(tail -n "$MONITORING_CONTENT_HEIGHT" "$file")
+        
         for line in "${lines[@]}"; do
-            printf "${COLOR_BORDER}|${RESET} %-*.*s ${COLOR_BORDER}|${RESET}\n" "$inner_width" "$inner_width" "$line"
+            printf "${COLOR_BORDER}│${RESET} %-*.*s ${COLOR_BORDER}│${RESET}\n" "$inner_width" "$inner_width" "$line"
         done
-        for ((j=${#lines[@]}; j<WINDOW_HEIGHT-2; j++)); do
-            printf "${COLOR_BORDER}|${RESET} %-*s ${COLOR_BORDER}|${RESET}\n" "$inner_width" ""
+        for ((j=${#lines[@]}; j<MONITORING_CONTENT_HEIGHT; j++)); do
+            printf "${COLOR_BORDER}│${RESET} %-*s ${COLOR_BORDER}│${RESET}\n" "$inner_width" ""
         done
-        printf "${COLOR_BORDER}+%*s+${RESET}\n" $((cols - 2)) '' | tr ' ' '-'
+
+        printf "${COLOR_BORDER}╰"
+
+        for ((i=0; i<cols-2; i++)); do printf "─"; done
+
+        printf "╯${RESET}"
+
         tput rc
         sleep 0.2
     done
