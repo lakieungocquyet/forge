cd4cb2ed8a0dee313fa4b03d60a0a0c267f983f5
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-04-07 22:35:37 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,11 @@
 SCRIPT_DIR_PATH="$(dirname "$(realpath $0)")"
 
 rm -rf "$SCRIPT_DIR_PATH/.pixi"
-rm -f pixi.toml
-rm -f pixi.lock
+rm -f "$SCRIPT_DIR_PATH/pixi.toml"
+rm -f "$SCRIPT_DIR_PATH/pixi.lock"
 
-# sed -i '/>>> forge shell hook >>>/,/<<< forge shell hook <<</d' ~/.bashrc
+pixi global uninstall forge
+
+sed -i '/>>> forge shell hook >>>/,/<<< forge shell hook <<</d' ~/.bashrc
 sed -i '/>>> added by forge installer >>>/,/<<< added by forge installer <<</d' ~/.bashrc
 
