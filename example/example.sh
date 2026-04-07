64dbddc5d6bdc0f5b35a5733e051365a5544052e
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-04-07 22:33:49 +0700

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -1,26 +1,26 @@
-forge call-variants \
+forge workflow call-variants \
     -I ~/GitHub/forge/example/input.yaml \
     -O ~/GitHub/forge/results \
-    -R ~/GitHub/forge/resources/hg19/reference_genome_hg19/hg19.p13.plusMT.no_alt_analysis_set.fa \
+    -R ~/resources/generated/reference_genome_hg19/processed/hg19.p13.plusMT.no_alt_analysis_set.fa \
     -r ~/GitHub/forge/resources/hg19/regions_hg19/s07604624_hg19/s07604624_covered.bed \
     --bqsr-known-sites \
-        ~/GitHub/forge/resources/hg19/variant_resources_hg19/1000g_phase1_indels_hg19/1000G_phase1.indels.hg19.sites.vcf.bgz \
-        ~/GitHub/forge/resources/hg19/variant_resources_hg19/dbsnp_138_hg19/dbsnp_138.hg19.vcf.bgz \
-        ~/GitHub/forge/resources/hg19/variant_resources_hg19/1000g_omni2_5_hg19/1000G_omni2.5.hg19.sites.vcf.bgz \
+        /home/lknq/resources/generated/1000g_phase1_indels_hg19/processed/1000G_phase1.indels.hg19.sites.vcf.bgz \
+        /home/lknq/resources/generated/dbsnp_138_hg19/processed/dbsnp_138.hg19.vcf.bgz \
+        /home/lknq/resources/generated/1000g_omni2_5_hg19/processed/1000G_omni2.5.hg19.sites.vcf.bgz \
     --annotation-resource \
-        dbsnp_138=~/GitHub/forge/resources/hg19/variant_resources_hg19/dbsnp_138_hg19/dbsnp_138.hg19.vcf.bgz \
-        phase1_1000g_indels=~/GitHub/forge/resources/hg19/variant_resources_hg19/1000g_phase1_indels_hg19/1000G_phase1.indels.hg19.sites.vcf.bgz \
-        omni2_5_1000g=~/GitHub/forge/resources/hg19/variant_resources_hg19/1000g_omni2_5_hg19/1000G_omni2.5.hg19.sites.vcf.bgz \
+        dbsnp_138=/home/lknq/resources/generated/dbsnp_138_hg19/processed/dbsnp_138.hg19.vcf.bgz \
+        phase1_1000g_indels=/home/lknq/resources/generated/1000g_phase1_indels_hg19/processed/1000G_phase1.indels.hg19.sites.vcf.bgz \
+        omni2_5_1000g=/home/lknq/resources/generated/1000g_omni2_5_hg19/processed/1000G_omni2.5.hg19.sites.vcf.bgz \
     --annotation-resource \
-        clinvar=~/GitHub/forge/resources/hg19/variant_resources_hg19/clinvar_20240716_hg19/clinvar_20240716.hg19.vcf.bgz \
-        dbnsfp=~/GitHub/forge/resources/hg19/variant_resources_hg19/dbnsfp4_9a_hg19/dbnsfp4.9a_hg19.txt.bgz \
-        esp6500si_v2_ssa137=~/GitHub/forge/resources/hg19/variant_resources_hg19/esp6500si_v2_ssa137_hg19/esp6500si_v2_ssa137.hg19.vcf.bgz \
-        phase3_1000g_v4_20130502=~/GitHub/forge/resources/hg19/variant_resources_hg19/1000g_phase3_v4_20130502_sites_hg19/1000G_phase3_v4_20130502.sites.hg19.vcf.bgz \
+        clinvar=/home/lknq/resources/generated/clinvar_20240716_hg19/processed/clinvar_20240716.hg19.vcf.bgz \
+        dbnsfp=/home/lknq/resources/generated/dbnsfp4_9a_hg19/processed/dbnsfp4.9a_hg19.txt.bgz \
+        esp6500si_v2_ssa137=/home/lknq/resources/generated/esp6500si_v2_ssa137_hg19/processed/esp6500si_v2_ssa137.hg19.vcf.bgz \
+        phase3_1000g_v4_20130502=/home/lknq/resources/generated/1000g_phase3_v4_20130502_sites_hg19/processed/1000G_phase3_v4_20130502.sites.hg19.vcf.bgz \
     -t 8 \
     --min-memory 8 \
     --max-memory 20
 
-# forge identify-hla-alleles \
-#     -I ~/GitHub/forge/example/input.yaml \
-#     -O ~/GitHub/forge/results \
-#     -t 8
+forge workflow identify-hla-alleles \
+    -I ~/GitHub/forge/example/input.yaml \
+    -O ~/GitHub/forge/results \
+    -t 8
