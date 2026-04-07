409c6ee0ecdf2a4921adfb24a867f5e70146ebb0
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-04-07 22:34:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
 SCRIPT_DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-pixi init .
+: << 'BLOCK'
+pixi init --format pixi
 pixi workspace channel add -m pixi.toml "bioconda"
 pixi workspace environment add forge
 
 pixi add --no-install \
-    bwa=* samtools=* gatk4=* bcftools=* snpeff=* snpsift=* t1k=* \
+    bwa=* samtools=* gatk4=* bcftools=* snpeff=* snpsift=* t1k=* optitype=* delly=* \
     jq=* python=* \
     pyyaml=* pandas=* xlsxwriter=* seaborn=* cyvcf2=*
-
-# pixi task add forge "python3 main.py" --description "Run Forge"
 
 pixi install -e forge
 
 echo "# >>> added by forge installer >>>" >> ~/.bashrc
-
-# echo 'alias forge="pixi run --quiet forge"' >> ~/.bashrc
 
 echo "# >>> forge shell hook >>>" >> ~/.bashrc
 pixi shell-hook --shell bash -e forge >> ~/.bashrc
@@ -25,6 +22,18 @@ chmod +x $SCRIPT_DIR_PATH/forge
 
 echo "# <<< added by forge installer <<<" >> ~/.bashrc
 
+source ~/.bashrc
+BLOCK
 
+pixi global install -e forge -c conda-forge -c bioconda \
+    fastp=* bwa=* samtools=* gatk4=* bcftools=* snpeff=* snpsift=* t1k=* seqkit=* optitype=* delly=* dicey=* tracy=* \
+    jq=* python=* \
+    pyyaml=* pandas=* xlsxwriter=* seaborn=* cyvcf2=*
+
+echo "# >>> added by forge installer >>>" >> ~/.bashrc
+echo "export PATH=\"$SCRIPT_DIR_PATH:\$PATH\"" >> ~/.bashrc
+echo "# <<< added by forge installer <<<" >> ~/.bashrc
+
+chmod +x $SCRIPT_DIR_PATH/forge
 source ~/.bashrc
 
