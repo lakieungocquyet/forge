99ecc8d08147cc4aee7763080e6c02c313aa1a27
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-04-12 12:40:57 +0700

    Update run_command_with_monitoring.sh

--- src/bash/utils/run_command_with_monitoring.sh
+++ src/bash/utils/run_command_with_monitoring.sh
@@ -13,8 +13,23 @@ run_command_with_monitoring() {
     # ) 2>&1 | stdbuf -oL tee -a "$monitoring_file" "$monitoring_stream_file" >/dev/null &
 
     (
+        set -Eeo pipefail
+        handle_error() {
+            local exit_code=$?
+            {
+                printf "\n"
+                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────"
+                printf "${red_color}[ERROR]${reset} File:      %s\n" "${BASH_SOURCE[1]:-$BASH_SOURCE[0]}"
+                printf "${red_color}[ERROR]${reset} Function:  %s\n" "${FUNCNAME[1]:-main}"
+                printf "${red_color}[ERROR]${reset} Line       %s\n" "${BASH_LINENO[0]}"
+                printf "${red_color}[ERROR]${reset} Command:   %s\n" "$(echo "${BASH_COMMAND}" | tr '\n' ' ' | tr -s ' ')"
+                printf "${red_color}[ERROR]${reset} Exit code: %s\n" "${exit_code}"
+                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────"
+            } >> "$monitoring_stream_file"
+        }
+        trap handle_error ERR
         $command
-    ) 2>&1 | tee -a "$monitoring_file" "$monitoring_stream_file" >/dev/null &
+    ) > >(tee -a "$monitoring_file" "$monitoring_stream_file" >/dev/null) 2>&1 &
 
     local command_pid=$!
 
@@ -24,14 +39,15 @@ run_command_with_monitoring() {
         "$workflow_status_file" \
         "$workflow_progress_file" &
     local window_pid=$!
-    
-    while kill -0 $command_pid 2>/dev/null; do
-        wait $command_pid 2>/dev/null
-    done
+
+    wait $command_pid 2>/dev/null
+    local command_exit_code=$?
 
     kill $window_pid 2>/dev/null
     wait $window_pid 2>/dev/null
+    echo "$command_exit_code"
+    if [ $command_exit_code -ne 0 ]; then
+        tail -n 8 $monitoring_stream_file
+    fi
 
-    tput rc
-    tput ed 
 }
