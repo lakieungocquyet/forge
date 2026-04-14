8f5f4be0c49b7274965a3cde58eb3e0a75b71d2b
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2026-04-14 09:01:00 +0700

    Update run_command_with_monitoring.sh

--- src/bash/utils/run_command_with_monitoring.sh
+++ src/bash/utils/run_command_with_monitoring.sh
@@ -3,11 +3,11 @@ run_command_with_monitoring() {
     
     local workflow_tilte="$2"
 
-    local monitoring_file="$3"
-    local monitoring_stream_file="$4"
-    local workflow_status_file="$5"
-    local workflow_progress_file="$6"
-
+    local workflow_console_log_file="$3"
+    local workflow_console_stream_file="$4"
+    local workflow_status_log_file="$5"
+    local workflow_progress_log_file="$6"
+    local workflow_error_log_file="$7"
     # (
     #     stdbuf -oL -eL "$command"
     # ) 2>&1 | stdbuf -oL tee -a "$monitoring_file" "$monitoring_stream_file" >/dev/null &
@@ -18,26 +18,26 @@ run_command_with_monitoring() {
             local exit_code=$?
             {
                 printf "\n"
-                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────"
+                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────\n"
                 printf "${red_color}[ERROR]${reset} File:      %s\n" "${BASH_SOURCE[1]:-$BASH_SOURCE[0]}"
                 printf "${red_color}[ERROR]${reset} Function:  %s\n" "${FUNCNAME[1]:-main}"
                 printf "${red_color}[ERROR]${reset} Line       %s\n" "${BASH_LINENO[0]}"
                 printf "${red_color}[ERROR]${reset} Command:   %s\n" "$(echo "${BASH_COMMAND}" | tr '\n' ' ' | tr -s ' ')"
                 printf "${red_color}[ERROR]${reset} Exit code: %s\n" "${exit_code}"
-                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────"
-            } >> "$monitoring_stream_file"
+                printf "${red_color}[ERROR]${reset} ────────────────────────────────────────────────\n"
+            } > "$workflow_error_log_file"
         }
         trap handle_error ERR
         $command
-    ) > >(tee -a "$monitoring_file" "$monitoring_stream_file" >/dev/null) 2>&1 &
+    ) > >(tee -a "$workflow_console_log_file" "$workflow_console_stream_file" >/dev/null) 2>&1 &
 
     local command_pid=$!
 
     render_monitoring_window \
         "$workflow_tilte" \
-        "$monitoring_stream_file" \
-        "$workflow_status_file" \
-        "$workflow_progress_file" &
+        "$workflow_console_stream_file" \
+        "$workflow_status_log_file" \
+        "$workflow_progress_log_file" &
     local window_pid=$!
 
     wait $command_pid 2>/dev/null
@@ -45,9 +45,9 @@ run_command_with_monitoring() {
 
     kill $window_pid 2>/dev/null
     wait $window_pid 2>/dev/null
-    echo "$command_exit_code"
+
     if [ $command_exit_code -ne 0 ]; then
-        tail -n 8 $monitoring_stream_file
+        cat $workflow_error_log_file
     fi
 
 }
